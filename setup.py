7e2120d02586c86247efdccbe2edb0f960a4c59b
Julien Phalip <jphalip@gmail.com>
2010-10-01 17:26:55 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glamkit-adminboost',
+    author='Julien Phalip',
+    author_email='julien@interaction.net.au',
+    version='0.0.1',
+    description='Makes the Django admin even more awesomer',
+    url='http://github.com/glamkit/glamkit-admintools',
+    packages=find_packages(),
+    package_data={
+    },
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)

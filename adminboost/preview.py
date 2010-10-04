af9356e9d89ac7b27238bd71f47d791a9481e626
Julien Phalip <jphalip@gmail.com>
2010-10-04 13:12:47 +1100

    Made preview more generic (can now be used for other things than just images)

--- adminboost/preview.py
+++ adminboost/preview.py
@@ -2,65 +2,84 @@ from django import forms
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 from django.contrib.admin.options import InlineModelAdmin
+from django.conf import settings
 
-from easy_thumbnails.files import get_thumbnailer
 
 
 # Admin classes ------------------------------------------------------------
 
-class ImagePreviewInline(InlineModelAdmin):
+class PreviewInline(InlineModelAdmin):
     def __init__(self, *args, **kwargs):
         # enforce use of fieldsets so we can inject the preview field
         if not self.fieldsets:
             self.__class__.fieldsets = (
                 (None, {'fields': self.form.base_fields}),)
-        super(ImagePreviewInline, self).__init__(*args, **kwargs)
+        super(PreviewInline, self).__init__(*args, **kwargs)
         # inject 'preview' after super init since it won't validate
         self.declared_fieldsets[0][1]['fields'] = ('preview',) + \
             tuple(self.declared_fieldsets[0][1]['fields'])    
 
-class ImagePreviewStackedInline(ImagePreviewInline):
+class PreviewStackedInline(PreviewInline):
     template = 'admin/edit_inline/stacked.html'
 
-class ImagePreviewTabularInline(ImagePreviewInline):
+class PreviewTabularInline(PreviewInline):
     template = 'admin/edit_inline/tabular.html'
 
 # Form classes ------------------------------------------------------------
     
-class ImagePreviewWidget(forms.widgets.Input):
+class PreviewWidget(forms.widgets.Input):
+    is_hidden = False
+    input_type = 'text'
+    
     def __init__(self, *args, **kwargs):
         self.instance = kwargs.pop('instance', None)
         self.form = kwargs.pop('form', None)
-        super(ImagePreviewWidget, self).__init__(*args, **kwargs)
+        super(PreviewWidget, self).__init__(*args, **kwargs)
+
+class ImagePreviewWidget(PreviewWidget):        
         
     def render(self, name, data, attrs={}):
+        from easy_thumbnails.files import get_thumbnailer
         if self.instance:
-            image = self.form.get_image(self.instance)
+            images = self.form.get_images(self.instance)
             options = dict(size=(120, 120), crop=False)
-            thumbnail = get_thumbnailer(image.file).get_thumbnail(options)
-            image_url = image.file.url
-            return mark_safe('<a href="%(image_url)s" target="_blank"><img src="%(thumbnail_url)s" class="feincmstools-thumbnail"/></a>' % {'image_url': image_url, 'thumbnail_url': thumbnail.url})
+            html = '<div class="adminboost-preview">'
+            for image in images:
+                thumbnail = get_thumbnailer(image.file).get_thumbnail(options)
+                image_url = image.file.url
+                html += '<div class="adminboost-preview-thumbnail"><a href="%(image_url)s" target="_blank"><img src="%(thumbnail_url)s"/></a></div>' % {'image_url': image_url, 'thumbnail_url': thumbnail.url}
+            html += '</div>'
+            return mark_safe(html)
         else:
             return ''
-
-class ImagePreviewField(forms.Field):
+        
+class PreviewField(forms.Field):
     """ Dummy "field" to provide preview thumbnail. """
     def __init__(self, *args, **kwargs):
         self.instance = kwargs.pop('instance', None)
         self.form = kwargs.pop('form', None)
-        kwargs['widget'] = ImagePreviewWidget(instance=self.instance, form=self.form)
-        super(ImagePreviewField, self).__init__(*args, **kwargs)
+        kwargs['widget'] = self.form.preview_widget_class(instance=self.instance, form=self.form)
+        super(PreviewField, self).__init__(*args, **kwargs)
 
-class ImagePreviewInlineForm(forms.ModelForm):
-    """ Add image preview. """
+class PreviewInlineForm(forms.ModelForm):
+    
     def __init__(self, *args, **kwargs):
-        super(ImagePreviewInlineForm, self).__init__(*args, **kwargs)
-        preview_field = ImagePreviewField(
+        super(PreviewInlineForm, self).__init__(*args, **kwargs)
+        preview_field = PreviewField(
             label=_('Preview'), required=False,
             instance=kwargs.get('instance', None), form=self)
         self.fields.insert(0, 'preview', preview_field)
         self.base_fields.insert(0, 'preview', preview_field)
         
-    def get_image(self, instance):
+    class Media:
+        css = { 
+            'all': ("%sadminboost/styles.css" % settings.MEDIA_URL,)
+            }
+    
+class ImagePreviewInlineForm(PreviewInlineForm):
+    
+    preview_widget_class = ImagePreviewWidget
+    
+    def get_images(self, instance):
         ''' This needs to be specified by the child form class, as we cannot anticipate the name of the image model field '''
         raise NotImplementedError

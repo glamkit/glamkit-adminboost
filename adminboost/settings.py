08cdcfd4efc4cd2877582967f7a9bf397511ec8f
Greg Turner <greg@interaction.net.au>
2012-09-19 21:14:15 +1000

    deprecating in favour of ixc

--- adminboost/settings.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-ADMINBOOST_PREVIEW_SIZE = getattr(settings, 'ADMINBOOST_PREVIEW_SIZE',
-    (300,300))
--- adminboost/urls.py
+++
@@ -1,4 +0,0 @@
-
-from django.contrib import admin
-
-urlpatterns = getattr(admin.site, '_inline_urls', {}).values()
